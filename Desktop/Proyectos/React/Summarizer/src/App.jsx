783cb1a54b029976c5500a973994ce7a8e66715b
arkhanis <casanovac@gmail.com>
2024-02-08 11:01:27 -0300

    set up

---
+++ Desktop/Proyectos/React/Summarizer/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <main>
+      <Hero />
+      <Demo />
+    </main>
+  )
+}
+
+export default App
